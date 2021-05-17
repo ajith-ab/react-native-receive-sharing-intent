fadcc8072f08497f6881dbf9495e2babf384eaaf
Ajith A B <ajith4ab@gmail.com>
2021-05-17 23:13:19 +0530

    2.0.0 released

--- lib/typescript/ReceiveSharingIntent.d.ts
+++ lib/typescript/ReceiveSharingIntent.d.ts
@@ -2,7 +2,9 @@ import type { IReceiveSharingIntent } from "./ReceiveSharingIntent.interfaces";
 declare class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     private isIos;
     private utils;
+    private isClear;
     getReceivedFiles(handler: Function, errorHandler: Function, protocol?: string): void;
+    clearReceivedFiles(): void;
     protected getFileNames(handler: Function, errorHandler: Function, url: string): void;
 }
 export default ReceiveSharingIntentModule;
