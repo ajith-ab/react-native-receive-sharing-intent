dd29cf514c3728d92285dee631146565e5f86c61
DaveLomber <admin@connectycube.com>
2022-04-19 12:21:01 +0300

    fixed getFileNames iOS

--- lib/typescript/ReceiveSharingIntent.d.ts
+++ lib/typescript/ReceiveSharingIntent.d.ts
@@ -3,6 +3,7 @@ declare class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     private isIos;
     private utils;
     private isClear;
+    private processedIOSFiles;
     getReceivedFiles(handler: Function, errorHandler: Function, protocol?: string): void;
     clearReceivedFiles(): void;
     protected getFileNames(handler: Function, errorHandler: Function, url: string): void;
