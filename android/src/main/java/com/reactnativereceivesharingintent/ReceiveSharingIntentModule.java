a495a29d08bc97bd409824b841f5a60f6bd6f8f0
Nikolay Stanchev <Nikolay Stanchev>
2022-10-18 12:00:29 +0300

    Fix setIntent(null) issue
    Setting the intent to null caused a NullPointerException
    in firebase-dynamic-links package due to the old version
    of it we are using. In addition, setting the intent to null
    in this way can cause issues with a lot of third party libraries
    and RN modules that rely on the intent.

--- android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
+++ android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 
-
 import android.os.Build;
 import androidx.annotation.RequiresApi;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
 
 public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
   public final String Log_Tag = "ReceiveSharingIntent";
@@ -26,31 +24,41 @@ public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
     receiveSharingIntentHelper = new ReceiveSharingIntentHelper(applicationContext);
   }
 
-
   protected void onNewIntent(Intent intent) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     mActivity.setIntent(intent);
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   @ReactMethod
-  public void getFileNames(Promise promise){
+  public void getFileNames(Promise promise) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
     receiveSharingIntentHelper.sendFileNames(reactContext, intent, promise);
-    mActivity.setIntent(null);
+    String action = intent != null ? intent.getAction() : null;
+    if (action != null &&
+        (action.equals(android.content.Intent.ACTION_SEND) ||
+            action.equals(android.content.Intent.ACTION_SEND_MULTIPLE))) {
+      intent.setAction(null);
+      mActivity.setIntent(intent);
+    }
   }
 
   @ReactMethod
-  public void clearFileNames(){
+  public void clearFileNames() {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
     receiveSharingIntentHelper.clearFileNames(intent);
   }
-
 
   @Override
   public String getName() {
